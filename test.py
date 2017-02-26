10180b00ff7628f5c2c3894e254287b076bf3f56
Lucas Maia <lucasluc4@hotmail.com>
2017-02-26 11:47:48 -0300

    Improved tests

--- test.py
+++ test.py
@@ -3,8 +3,17 @@ from hell_triangle import solve_hell_triangle
 
 class TestStringMethods(unittest.TestCase):
 
+    def test_solution_for_one_element(self):
+        self.assertEqual(solve_hell_triangle(0,0,[[6]]), 6)
+
     def test_solution(self):
         self.assertEqual(solve_hell_triangle(0,0,[[6],[3,5],[9,7,1],[4,6,8,4]]), 26)
+
+    def test_raises_exception_if_no_array_is_provided(self):
+        self.assertRaises(TypeError, solve_hell_triangle, 0, 0, 6)
+
+    def test_raises_exception_if_no_sub_array_is_provided(self):
+        self.assertRaises(TypeError, solve_hell_triangle, 0, 0, [6])
 
     def test_raises_exception_if_row_sizes_are_wrong(self):
         self.assertRaises(ValueError, solve_hell_triangle, 0, 0, [[6],[9,7,1],[4,6,8,4]])
